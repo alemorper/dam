c3466b9c4b647d1f453d21940ef96dbc95c2eb78
Alejandro alemorper4 <amorenoperez.214@gmail.com>
2024-05-06 17:48:52 +0200

    Login Añadido

--- app/src/main/java/com/dam/fitmaster/MainActivity.java
+++ app/src/main/java/com/dam/fitmaster/MainActivity.java
@@ -3,12 +3,30 @@ package com.dam.fitmaster;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        Button LoginButton = findViewById(R.id.LoginButton);
+        Button SignupButton = findViewById(R.id.SignupButton);
+        EditText UserText = findViewById(R.id.UserText);
+        EditText PasswordText = findViewById(R.id.PasswordText);
+
+
+        LoginButton.setOnClickListener(view -> {
+            // Acción al hacer clic en el botón de inicio de sesión
+            String username = UserText.getText().toString();
+            String password = PasswordText.getText().toString();
+            Log.d("MainActivity", "Nombre de usuario: " + username);
+            Log.d("MainActivity", "Contraseña: " + password);
+        });
+
+        }
 }
