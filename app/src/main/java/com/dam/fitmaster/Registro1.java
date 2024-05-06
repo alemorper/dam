89a49aff8de57e6dc8c290e09eea9ebf2fac710a
carmuntor <mtcarlos3b@gmail.com>
2024-05-06 19:45:44 +0200

    Versión con estilo registro1

---
+++ app/src/main/java/com/dam/fitmaster/Registro1.java
@@ -0,0 +1,14 @@
+package com.dam.fitmaster;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Registro1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro1);
+    }
+}
