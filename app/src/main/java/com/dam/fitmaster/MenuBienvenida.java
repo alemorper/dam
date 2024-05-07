05382c63c592492c8054446316596e8008363a58
carmuntor <mtcarlos3b@gmail.com>
2024-05-07 19:56:08 +0200

    Menú de bienvenida al usuario exo

--- app/src/main/java/com/dam/fitmaster/MenuBienvenida.java
+++ app/src/main/java/com/dam/fitmaster/MenuBienvenida.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Registro1 extends AppCompatActivity {
+public class MenuBienvenida extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_registro1);
+        setContentView(R.layout.activity_menu_bienvenida);
     }
 }
